0453080770b7a374df03880b010c894fb188c4ee
harvey <1072412842@qq.com>
2020-03-27 15:20:32 +0800

    第一次修改

--- src/N叉树/MaximumDepthofNaryTree.java
+++ src/N叉树/MaximumDepthofNaryTree.java
@@ -5,6 +5,7 @@ package N叉树;
  * @date 2020/3/27
  * @slogan Drive business with technology, make business generate value.
  */
+
 public class MaximumDepthofNaryTree {
     int ans = 1;
     public int maxDepth(Node root) {
