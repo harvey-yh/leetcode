9c98972b79525d231ece0e1e09f07fdb1c4189b0
harvey <1072412842@qq.com>
2020-03-30 17:38:33 +0800

    面试题

---
+++ src/面试题/圆圈中最后剩下的数字.java
@@ -0,0 +1,45 @@
+package 面试题;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+
+/**
+ * @author Harvey
+ * @date 2020/3/30
+ * @slogan Drive business with technology, make business generate value.
+ */
+public class 圆圈中最后剩下的数字 {
+
+    public static int lastRemaining(int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int tmp=-1;
+        int pointer = m-1;
+        while(!list.isEmpty()){
+            while(pointer >= list.size()){
+                pointer -= list.size();
+            }
+
+            tmp = list.get(pointer);
+            list.remove(pointer);
+            pointer += m-1;
+        }
+        return tmp;
+    }
+
+    public int lastRemaining1(int n, int m) {
+        int flag = 0;
+        for(int i=2;i <= n;i++){
+            flag = (flag + m) % i;
+        }
+        return flag;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println(lastRemaining(5,3));
+    }
+
+}
